feb3aac2559bd53d90f93a89337b6c8e440ee0e0
Hanna-Ismahilova <39468327+Hanna-Ismahilova@users.noreply.github.com>
2019-10-21 10:22:04 +0200

    Moved project files to git

---
+++ UIAtomationFramework/Base/ABSBaseUITest.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UIAtomationFramework.Base
+    {
+    public abstract class ABSBaseUITest
+        {
+        private IWebDriver _webDriver;
+
+        public ABSBaseUITest (IWebDriver webDriver )
+            {
+            _webDriver = webDriver;
+            }
+
+        //create loop which will searching according to set time + assertation if not found
+        public IWebElement FinderElement (By locator )
+            {
+            return _webDriver.FindElement(locator);
+            }
+
+        #region Clicks
+        public void SafeClick ( By locator)
+            {
+            FinderElement(locator).Click();
+            }
+        #endregion
+        }
+    }
---
+++ UIAtomationFramework/Base/BaseUITest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace UIAtomationFramework.Base
+    {
+    public class BaseUITest : ABSBaseUITest
+        {
+        public BaseUITest ( IWebDriver webDriver ) : base(webDriver)
+            {
+
+            }
+        }
+    }
