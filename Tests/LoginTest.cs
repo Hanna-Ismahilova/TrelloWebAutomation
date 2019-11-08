9b4908a5f93e9b334302e0e28bd0d03cec8b6679
Hanna-Ismahilova <39468327+Hanna-Ismahilova@users.noreply.github.com>
2019-11-08 20:01:07 +0100

    Add files via upload

---
+++ Tests/LoginTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UIAtomationFramework.Base.Models;
+using UIAtomationFramework.Helpers;
+
+namespace TrelloWebAutomation.Tests
+    {
+    [TestFixture] 
+
+    public class LoginTest
+        {
+        IWebDriver _driver;
+
+        [SetUp]   
+        public void SetUp ( )
+            {
+
+            AppSettingsModel appSettingsModel = AppConfig.Load();
+            _driver = new ChromeDriver(@"C:\Users\Hanna_Ismahilova\source\repos\TrelloWebAutomation\TrelloWebAutomation\bin\Debug\netcoreapp2.2\chromedriver.exe\");
+            _driver.Url = appSettingsModel.URL;
+          
+
+            }
+        
+        [TearDown]
+        public void TeatDown ( )
+            {
+
+            }
+
+        [Test]
+        public void Test001 ( )
+            {
+
+
+            }
+
+        }
+    }
