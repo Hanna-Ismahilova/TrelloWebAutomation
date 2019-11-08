e13adb6b66d0adca6c157071a3ba410b3a9d488d
Hanna-Ismahilova <39468327+Hanna-Ismahilova@users.noreply.github.com>
2019-11-08 20:16:25 +0100

    Add files via upload

---
+++ UIAtomationFramework/Base/Models/AppSettingsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UIAtomationFramework.Base.Models
+    {
+    public class AppSettingsModel
+        {
+
+            public string URL { get; set; }
+            public string Login { get; set; }
+            public string Password { get; set; }
+            public string Browser { get; set; }
+            
+        }
+    }
---
+++ UIAtomationFramework/Helpers/AppConfig.cs
@@ -0,0 +1,31 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UIAtomationFramework.Base.Models;
+
+namespace UIAtomationFramework.Helpers
+    {
+   public static class AppConfig
+        {
+
+        public static AppSettingsModel Load ( )
+            {
+
+            using (var stream = new StreamReader(@"C:\Users\Hanna_Ismahilova\source\repos\TrelloWebAutomation\TrelloWebAutomation\appSettings.json") )
+                {
+
+                AppSettingsModel appSettings = JsonConvert.DeserializeObject<AppSettingsModel>(stream.ReadToEnd());
+
+                return appSettings;
+                }
+                //var Files = Directory.GetFiles(".\\");
+
+
+            }
+
+
+        }
+    }
